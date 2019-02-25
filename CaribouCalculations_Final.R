501f0276167ebab05f4744f483922cecdd5405d6
fstewart <fstewart@nrn.nrcan.gc.ca>
2019-02-25 11:39:35 -0800

    deisgnation of habitat set assides set to 0.95

--- CaribouCalculations_Final.R
+++ CaribouCalculations_Final.R
@@ -27,7 +27,7 @@
 # Set the barriers here -- this would be the numbers that go into the BRAT figure
 Threat1_barriers <- c(quote(Threat1_barrier_1), 1.40, 0.65, 1.5, 1.0, 0.9, 1.0) # in units of Initial Frequency
 Threat2_barriers <- quote(c(Threat2_barrier_1)) # in units of Initial Frequency # This is derived from data below
-Threat3_barriers <- c(1.05, 1, 1) # in units of Initial Frequency
+Threat3_barriers <- c(0.95, 1, 1) # in units of Initial Frequency
 Threat4_barriers <- c(1.05, 1.05, 1.05, 1.00, 1.00)
 
 ###############################################################################
@@ -327,7 +327,7 @@ print(PostMitigate_lambda <- 1 + (1-topEvent) < 1 + (1-postMitigate))
 # Step 8b: look at different management scenarios by changing the alternate value to equal 1 (i.e. no effect)
 # un comment below lines to look at these strategies, and how the postmitigate value changes
 
-postMitigate <- postMitigate(topEvent, mitigate(1, 0.95, 1)) # this is the maternity pen lever - acting solo # 1.16
+#postMitigate <- postMitigate(topEvent, mitigate(1, 0.95, 1)) # this is the maternity pen lever - acting solo # 1.16
 #postMitigate <- postMitigate(topEvent, mitigate(0.814, 1, 1)) # this is the wolfcull lever - acting solo
 #postMitigate <- postMitigate(topEvent, mitigate(1, 1, 0.95)) # this is seismic lines - acting solo
 #postMitigate <- postMitigate(topEvent, mitigate(1, 0.95, 0.95)) # maternity penning and linear restoration
