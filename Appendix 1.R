3d4c51dbf39467b7d8c2f859ccc0ba32a63dcabc
fstewart <fstewart@nrn.nrcan.gc.ca>
2019-04-18 14:04:12 -0700

    graphs for appendix 1, resulting from revisions

---
+++ Appendix 1.R
@@ -0,0 +1,11 @@
+#################################################################################
+# Caribou calculations
+# for BRAT paper: Winder et al. in prep at Frontiers in Ecology and Evolution
+# Code that outlines the appendix - maybe make into an Rmd?
+# Started: April 2019
+# Last revised: April 18, 2019
+# Author: Frances Stewart
+#################################################################################
+
+# draw a simple normal distribution, with mean = 1 and sd = 0.1, as in the manuscript
+# to compare plot the lambda values on
