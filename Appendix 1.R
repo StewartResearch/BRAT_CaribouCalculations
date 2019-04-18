ced9af617344da3f1dc0ba1fdf226276cd2eecdc
fstewart <fstewart@nrn.nrcan.gc.ca>
2019-04-18 14:45:32 -0700

    final chanves to Final_Final, and clean up

--- Appendix 1.R
+++ Appendix 1.R
@@ -7,5 +7,38 @@
 # Author: Frances Stewart
 #################################################################################
 
+
 # draw a simple normal distribution, with mean = 1 and sd = 0.1, as in the manuscript
-# to compare plot the lambda values on
+# to plot the lambda values on for the Appendix lambda figure
+
+install.packages("cowplot")
+library(cowplot)
+
+# A) Section 2.6.1.3 Threat 1, Barrier 3: Wolf trapping and hunting
+p1 <- ggplot(data = data.frame(x = c(-1, 1)), aes(x)) +
+  stat_function(fun = dnorm, n = 101, args = list(mean = 0, sd = 0.1), geom = "area") + ylab("") + 
+  scale_y_continuous(breaks = NULL) +
+  xlab("lambda")
+p1
+# add sd vertical lines to this plot
+p2 <- p1 + geom_vline(xintercept = 0.1, linetype = 'dashed', size  = 0.6, colour = "White") + geom_vline(xintercept = -0.1, linetype = 'dashed', size = 0.6, color = "white")
+p2
+# add a line for the lambda value
+p3 <- p2 + geom_vline(xintercept = -0.019, linetype = 'dashed', size  = 1, colour = 'red')
+p3
+ggsave(p3, plot = ggplot2::last_plot(), device = NULL)
+
+
+# B) Section 2.6.1.4 Threat 1, Barrier 4: Management of early seral stage forests for cover to reduce access
+g1 <- ggplot(data = data.frame(x = c(-1, 1)), aes(x)) +
+  stat_function(fun = dnorm, n = 101, args = list(mean = 0, sd = 0.1), geom = "area") + ylab("") + 
+  scale_y_continuous(breaks = NULL) +
+  xlab("lambda")
+g1
+# add sd vertical lines to this plot
+g2 <- p1 + geom_vline(xintercept = 0.1, linetype = 'dashed', size  = 0.6, colour = "White") + geom_vline(xintercept = -0.1, linetype = 'dashed', size = 0.6, color = "white")
+g2
+# add a line for the lambda value
+g3 <- p2 + geom_vline(xintercept = 0.027, linetype = 'dashed', size  = 1, colour = 'red')
+g3
+ggsave(p3, plot = ggplot2::last_plot(), device = NULL)
--- CaribouCalculations_Final_Final.R
+++ CaribouCalculations_Final_Final.R
@@ -3,10 +3,8 @@
 # for BRAT paper: Winder et al. in prep at Frontiers in Ecology and Evolution
 # Started: January 2019
 # Last revised: April 18, 2019
-# By Eliot McIntire and Frances Stewart
+# authors: Eliot McIntire and Frances Stewart
 #################################################################################
-
-# TODO: double check all code and comments
 
 # Objectives: ----
 ### Calculate target frequency for the critical event
@@ -20,6 +18,8 @@
 ## Overall, we want to determine if a herd is above the 40% threshold (ie in the 60% probability of survival category)
 ## We therefore set 60% as our target threshold to achieve.
 
+## Through out, assumptions are stated as "ASSUMPTION". Search for this word too see them all.
+
 
 ###########################
 ###########################
@@ -28,6 +28,7 @@
 
 #########
 # Set the barriers here -- this would be the numbers that go into the BRAT figure
+# you might have to conduct several of the below steps before dpoing this one, if the barriers are not estimated from literature.
 Threat1_barriers <- c(quote(Threat1_barrier_1), 1.4, 0.65, 1.5, 1.0, 0.9, 1.0) # in units of Initial Frequency
 Threat2_barriers <- quote(c(Threat2_barrier_1)) # in units of Initial Frequency # This is derived from data below
 Threat3_barriers <- c(1.00, 1.00, 1.00) # in units of Initial Frequency
@@ -44,12 +45,12 @@ Threat4_barriers <- c(1.05, 1.05, 1.05, 1.00, 1.00)
 ### For Chinchaga: data obtained from the ECCC 2011 scientific report
 N = 250 # population of the Chinchaga herd as of 2008
 SadF = 0.87 # Adult female survival 
-recr = 0.13/2 # Juvenile female recruitment 
+recr = 0.13/2 # Juvenile female recruitment (assuming a 50:50 sex ratio) 
 sd1 <- 0.1 # ASSUMPTION # this value can go as high as 0.3, from the literature.
 
 # invert these numbers to calculate the mortality values:
 MortSadF = (1 - SadF) # part of this will go into the Initial frequency of the threats
-Mortrecr = (1 - recr) # part of this will go into the Initial frequency of the threats, only 1/2 the animals are female
+Mortrecr = (1 - recr) # part of this will go into the Initial frequency of the threats
 
 
 
@@ -62,12 +63,12 @@ Mortrecr = (1 - recr) # part of this will go into the Initial frequency of the t
 ## (whether initial, or current top event) for each threat.
 
 ##### The literature generally says that most of the mortality is due to predation, 
-## and that the majority of this is on juveniles (Hervieux et al. 2014; Wittmer et al. 2005 a and b, etc). 
+## and that the majority of this is on juveniles (Hervieux et al. 2014; Wittmer et al. 2005 a and b). 
 #####
 
 Threat1_multiplier = MortSadF * 0.90 # ASSUMPTION # I'm assuming that 90% of the variation in SadF is due to predation. This can be changed.
 #Threat2_multiplier = We calculate this amont below, and name it 'Difference_nonpredation', so we do not include it here.
-Threat3_multiplier = MortSadF * 0.05 #+ Mortrecr * 0.05 # ASSUMPTION # we split the remaining 10% of the variation in SadF between the final two threats. We also split the remaining 5% of rec between the final two threats.
+Threat3_multiplier = MortSadF * 0.05 # ASSUMPTION # we split the remaining 10% of the variation in SadF between the final two threats. We also split the remaining 5% of rec between the final two threats.
 Threat4_multiplier = Threat3_multiplier # ASSUMPTION
 
 # Assumptions about caribou population demongraphics, from published literaure, reports, and pers. comms:
@@ -109,20 +110,20 @@ print(lambdaQuartile)
 # The values need to come from the best sourced data, and when those data are limited, they  can be hard to determine.
 
 # However, what is important to realize is that the BRAT framework is adaptable and flexible as new information becomes 
-# available. Our manuscript here is show how, and can be changed as new information comes to light - not only the number 
+# available. Our manuscript here is showhow, and can be changed as new information comes to light - not only the number 
 # of threats or barriers to a threat, can be changed, but the associations between these items can be changed too.
 
 ####### Current top event frequencies are from known data 
-## (ie. known mortality or recruitment from ECCC data)
+## (ie. known mortality or recruitment from Environment Canada 2008; Environment Canada 2012 data)
 ####### Initial event frequencies are things that we need to find out (i.e. calculate)
 ## (ie. what was the frequency of this threat if there had been no landscape changes)
 
 
 # Threat 1 - General predation
 ### Current Top Event Frequency
-# some herds have very low adult female mortality. Thorugh trial and error with Snake and the Averaged herds, we discovered that
+# some herds have very low adult female mortality. Thorugh trial and error with Snake and the Averaged herd, we discovered that
 # if MortSadF is less than 0.1 (i.e less than 10% of adult females are dying) the effectiveness of wolf controls was listed as > 100%.
-# this just didnt make any sense. We therefore assume # ASSUMPTION # that 10% represents an ecological threshold of other processes
+# This just didnt make any sense. We therefore assume # ASSUMPTION # that 10% represents an ecological threshold of other processes
 # dominating adult female survival. We discovered that if we limit the initial frequency of adult female mortality to only 40%
 # by predation, the effectiveness of wolf controls made sense (i.e. less than 100%) across all study situations. We therefore assume
 # that if less than 10% of females are dying, 4% will be due to wolf predation, and the other 6% will be due to other causes (highlighted
@@ -130,17 +131,17 @@ print(lambdaQuartile)
 if(MortSadF < 0.10) {
   Threat1_InitialFreq <- MortSadF*0.4
 
-  } else { # if not, we back calculated the required threat value:
+  } else { # if not, we back-calculated the required threat value:
 Threat1_InitialFreq <- 0.05549 # ASSUMPTION
-# This calculation involved back calculating from the wolf mitigation initial frequency, to the juvenile predation threat frequency,
-## translating this value into lambda units of wolf predation and compensatory predation, calculating the ratio between these
-## predation effects, and applying the ratio to adult wolf vs compensatory predation. From there we could substitute in the current
-## top event frequency (known from ECCC data), to get this inital frequency value.
+# This calculation involved back calculating from the wolf mitigation initial frequency (0.814), to the juvenile predation threat frequency,
+# translating this value into lambda units of wolf predation and compensatory predation, calculating the ratio between these
+# predation effects, and applying the ratio to adult wolf vs compensatory predation. From there we could substitute in the current
+# top event frequency (known from Environment Canada data), to get this inital frequency value.
 # ASSUMPTION: the effectiveness of wolfculls extends from juveniles to adults equally
 # ASSUMPTION: This is a constant applied to all populations to determine the threat values of adult predation (as long as adult female
 # mortality is > 10%. See above portion of the IF statement). 
 ### This might be a big assumption! But it is mitigated by the above IF statement.
-###### See how we calculated this at the end of the script.
+###### See how we calculated this at the end of the script ('Other Information').
 }
 
 
@@ -148,23 +149,18 @@ Threat1_barrier_1 <-  Threat1_multiplier / (Threat1_InitialFreq * prod(unlist(Th
 Threat1_topEvent <- Threat1_multiplier # proportion of caribou mortality rate due to predation, rather than total adult female mortality
 Threat1_InitialFreq <- Threat1_topEvent/prod(sapply(Threat1_barriers, eval))
 
-### Initial Frequency
-##### calculated from the BRAT threat line:
-# predationAdultRev <- function(Threat1_InitialFreq, avoid, seismic, huntPred, earlySeral, huntCaribou, huntAltPrey) { # calculate the initial frequency of predation given the current frequency and the values of the thresholds
-#   TopEventFreq/ (avoid * seismic * huntPred * earlySeral * huntCaribou * huntAltPrey)
-# }
 
 
 # Threat 2 - Juvenile predation
 ### Initial Frequency
 ##### (i.e. rather all else being equal, how many cows produce a calf a year later if predation is a factor)
 # Initial frequency = Pregnancy Rate*Adult female survival*Calf survival to Day 30
-Threat2_InitialSurv <- pregR * SadF * surv1stDay # proportion of females that produce a female  calf
+Threat2_InitialSurv <- pregR * SadF * surv1stDay # proportion of females that produce a femalecalf
 Threat2_InitialFreq <- (1 - (Threat2_InitialSurv)) # proportion of females at the start of a year that result in no female calf a year later
 
 ### Current Top Event Frequency
-##### Difference between the calculated Initial Frequency, and the ECCC reported value (i.e. how 
-#  many female calf deaths from day 1 to a population survey are unaccounted for?)
+##### Difference between the calculated Initial Frequency, and the Environment Canada reported value 
+#   (i.e. how many female calf deaths from day 1 to a population survey are unaccounted for?)
 PropMortDay30ToSurvey = Mortrecr - Threat2_InitialFreq 
 # We know from maternal penning data that even between day 2 and day 30, 90% of calves survive 
 #   (i.e. even without predation, 90% of calves survive, and 10% die from "other causes"; Scott McNay pers comm - Klinse-Za meternity pen)
@@ -218,7 +214,7 @@ wolfCullEffect <- 0.186 # DATA/ASSUMPTION
 # i.e. lambda has the potential to increase by 18.6% during a full wolf cull.
 # Hervieux et al. (2014) found lambda increased between 4.6 (within herds) and 14% (between herds) during a 50% wolf cull. 
 # We avereaged these fundings, and standardized by 50% wolf cull to get the 18.6%
-# this assumes a linear relationship
+# this assumes a linear relationship # ASSUMPTION #
 
 #############################################################################################################################
 # Step 5b
@@ -226,7 +222,7 @@ wolfCullEffect <- 0.186 # DATA/ASSUMPTION
 # (or "other" predators, OR wolves that are not being culled)
 # i.e. this could include wolf immigration and source/sink dynamics -> an important discussion point.
 
-# We wanted to state this in terms of lmabda units
+# We wanted to state this in terms of lambda units
 Threat_LambdaEffect[[2]]
 #  So, 0.444 is predator effect on calves -- partition into "non-compensated wolf" == "True effect of wolves on calves" 
 #     and "other" e.g., 0.186 is max possible... partition amongst adults and juvs e.g., 0.9 and 0.1 -->
@@ -235,7 +231,7 @@ Threat_LambdaEffect[[2]]
 #     i.e. Threat_LambdaEffect[[1]] - (0.1*wolfcullEffect)
 #  "other predation" for adults = 
 
-# ASSUMPTION # we assumed that the majority of predation happend on juveniles (Hervieux et al. 2014)
+# ASSUMPTION # we assumed that the majority of predation happend on juveniles (Hervieux et al. 2014) - 90% of predation happened on juveniles
 wolfCullPropOnAdults <- 0.1
 wolfCullPropOnJuvs <- 1 - wolfCullPropOnAdults
 # therefore: 
@@ -259,15 +255,15 @@ effectivenessJuvs <- wolvesOnJuvs/(otherOnJuvs + wolvesOnJuvs)
 allOnAdults <- wolvesOnAdults/effectivenessAdults
 otherOnAdults <- allOnAdults - wolvesOnAdults
 
-
-a <- (otherOnAdults + Threat1_InitialFreq[1]) / Threat1_InitialFreq[1]
+# Double checking out calculations:
+a <- (otherOnAdults + Threat1_InitialFreq[1]) / Threat1_InitialFreq[1] # Lambda units
 #   In InitialEvent units --> (0.277 + Threat2_InitialFreq) / Threat2_InitialFreq
-b <- (otherOnJuvs + Threat2_InitialFreq[1]) / Threat2_InitialFreq[1]
+b <- (otherOnJuvs + Threat2_InitialFreq[1]) / Threat2_InitialFreq[1] # Lambda units
 #   Wolf part In InitialEvent units --> ((0.0186 - 0.07375) + Threat1_InitialFreq) / Threat1_InitialFreq
-d <- ((wolvesOnAdults) + Threat1_InitialFreq[1]) / Threat1_InitialFreq[1]
+d <- ((wolvesOnAdults) + Threat1_InitialFreq[1]) / Threat1_InitialFreq[1] # Lambda units
 #   Wolf part In InitialEvent units --> ((0.444584 - 0.3185) + Threat2_InitialFreq) / Threat2_InitialFreq
 # Wolf part In InitialEvent units --> 
-f <- ((wolvesOnJuvs) + Threat2_InitialFreq) / Threat2_InitialFreq
+f <- ((wolvesOnJuvs) + Threat2_InitialFreq) / Threat2_InitialFreq # Lambda units
 
 # This should be the effect on adults in initial units
 Threat1_barrier_1 <- (a - 1)  + (d - 1)  + 1 # in initial units
@@ -285,8 +281,6 @@ sapply(Threat1_barriers, eval)[[1]] # :)
 ########################
 # Step 6: Calculate the Current Total Top Event and Current Consequency (i.e. Mitigate) values ----
 # these values can be changed to better reflect data as it becomes available:
-### the first number of each function is the probability that the threat affects the citical event (lambda below zero)
-### the subsequent numbers of each function are the probability that the barrers will FAIL to prevent the threat
 
 # Top Event Frequency Function
 # calculate the top event frequency, before mitigation actions take place.
@@ -333,7 +327,6 @@ postMitigateS <- postMitigate(topEvent, mitigate(mitigate_cull, mitigate_restora
 message("This is the consequence frequency: ", postMitigateS)
 
 # Convert these inital frequency to values of lambda:
-
 Mitigation_LambdaEffect <- list()
 Mitigation_LambdaEffect[[1]] <- topEvent * (1 - mitigate_restoration) # linear restoration mitigation value, in Lambda units
 Mitigation_LambdaEffect[[2]] <- topEvent* (1 - mitigate_cull) # wolf cull mitigation value, in Lambda units
@@ -352,7 +345,7 @@ message("This is the consequence lambda: ", (1 + (1-postMitigateS)))
 #postMitigateS <- postMitigate(topEvent, mitigate(1, 1, mitigate_restoration)) # this is seismic lines - acting solo
 #postMitigateS <- postMitigate(topEvent, mitigate(1, mitigate_penning, mitigate_restoration)) # maternity penning and linear restoration
 
-message("This is the consequence lambda after mitigation: ", (1 + (1-postMitigateS)))
+#message("This is the consequence lambda after mitigation scenarios: ", (1 + (1-postMitigateS)))
 
 #################################################################################################################################
 # Step 9: Print the BRAT table, with values ----
@@ -366,8 +359,7 @@ library(diagram)
 # creates an empty plot
 openplotmat()
 
-# TODO: make this into a PDF that gets printed.
-
+pdf("BRAT_ThreatsAndBarriers.pdf")
 # create the coordinates
 # I want the boxes arranged in a 8, 2, 4, 6 formation (for Threat names/initial/top values, and one for each barrier)
 pos <- coordinates(c(8,2,4,6))
@@ -519,10 +511,12 @@ for(i in 1:length(my_labels)) {
   }
 }
 # make sure to Zoom on the plot to expand the figure enough to see all the numbers clearly
+dev.off()
 ############################################################################################
 
 # Hazard, mitigation, and consequence portion of the BRAT diagram
 
+pdf("BRAT_HazardMitigationConsequence.pdf")
 # creates an empty plot
 openplotmat()
 # create the coordinates
@@ -588,6 +582,7 @@ for(i in 1:length(my_labels)) {
   } 
 }
 # make sure to Zoom on the plot to expand the figure enough to see all the numbers clearly
+dev.off()
 #############################################################################################################################
 # Step 9b: Or you can just print out each value on its own
 
@@ -693,8 +688,10 @@ recr = ((0.072 + 0.139)/2)/2 # Juvenile female recruitment
 sd1 <- 0.1 # ASSUMPTION # this value can go as high as 0.3, from the literature.
 
 ##################################################################################################################################
-# the results of this work is summarized in Tables 2 and 4 of the current BRAT manuscript (Winder et al. 2019)
+# the results of this work is summarized in Tables 1 and 2 of the current BRAT manuscript (Winder et al. 2019)
 ##################################################################################################################################
+
+
 
 ###################################################################################################################################
 # Other information: ----
