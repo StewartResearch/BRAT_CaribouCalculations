72f7b8a7b560c216fc8f0eefb25a1d5ac94476fc
StewartResearch <fecstewart@gmail.com>
2019-12-03 11:26:06 -0800

    Added lambdaQuartile

--- BRATcalculator.R
+++ BRATcalculator.R
@@ -3,11 +3,21 @@
 # we are assuming that all values generated and input in BRAT() are correct.
 # the hard coded values here are taken directly from the BRAT figure in Winder et al.
 
-BRATcalc<-function(Threat1_InitialFrequency, Threat1_barrier2,
+BRATcalc<-function(Threat1_InitialFreq, Threat1_barriers,
                    Threat2_InitialFreq, Threat2_barriers, 
                    Threat3_InitialFreq, Threat3_barriers, 
                    Threat4_InitialFreq, Threat4_barriers)
   {
+#calculate the lambda quiartile we would like to use.
+  lambdaQuartile <- qnorm(0.6, mean = 1, sd1) # mortality quartile is at the 60% mark of this distribution. 
+# in other words, what lambda vlaue do herds need to accomplish to obtain sustainability according to ECCC's 2012 40% threshold.
+# Put this lambdaQuartile into the Target frequency of the Hazard box
+### This is the frequency we need to try and aim above, to prevent lambda being consistently below 1, given a population sd of 0.1
+# in this case it is:
+print(lambdaQuartile) 
+# 1.025
+  
+  
 # Top Event Frequency Function
 # calculate the top event frequency, before mitigation actions take place.
   topEventCalculator <- function(...) {
